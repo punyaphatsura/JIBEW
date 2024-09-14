3d1a2cc69bb6341b8e4bd586522d24b60d870738
Punyaphat Surakiatkamjorn <punyaphat.sura@gmail.com>
2024-09-15 04:50:17 +0700

    Add reset button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,15 @@
 
 import { useState } from 'react';
 import { useDropzone } from 'react-dropzone';
+import Image from 'next/image';
 
 export default function Home() {
   const [files, setFiles] = useState<File[]>([]);
   const [lastClicked, setLastClicked] = useState(-1);
   const [jsonData, setJsonData] = useState<
     | {
-        [fileName: string]: string; // The key is the file name and value is the combined header + base64
+        fileName: string; // The key is the file name and value is the combined header + base64
+        content: string; // The content
       }[]
     | null
   >(null);
@@ -37,7 +39,8 @@ export default function Home() {
         const base64String = await convertToBase64(file);
         const header = `data:${file.type};base64,`;
         return {
-          [file.name]: header + base64String.split(',')[1], // Remove redundant header in base64 string
+          fileName: file.name,
+          content: header + base64String.split(',')[1], // Remove redundant header in base64 string
         };
       })
     );
@@ -68,6 +71,12 @@ export default function Home() {
     });
   };
 
+  const reset = () => {
+    setFiles([]);
+    setJsonData(null);
+    setLastClicked(-1);
+  };
+
   return (
     <div className="min-h-screen bg-gray-100 p-6 flex flex-col items-center justify-center">
       <h1 className="text-3xl font-bold text-gray-800 mb-6">
@@ -75,7 +84,7 @@ export default function Home() {
       </h1>
       <div
         {...getRootProps()}
-        className={`w-full max-w-lg p-6 border-2 border-dashed rounded-lg cursor-pointer transition ${
+        className={`w-full max-w-[1000px] flex items-center justify-center h-52 p-6 border-2 border-dashed rounded-lg cursor-pointer transition ${
           isDragActive
             ? 'border-blue-500 bg-blue-100'
             : 'border-gray-300 bg-white'
@@ -91,17 +100,35 @@ export default function Home() {
       </div>
 
       {files.length > 0 && (
-        <div className="w-full max-w-lg bg-white p-4 rounded-lg shadow-md mt-4">
-          <h2 className="text-xl font-semibold text-gray-700 mb-4">
-            Selected Files
-          </h2>
-          <ul className="mb-4">
+        <div className="w-full max-w-[1000px] bg-white p-4 rounded-lg shadow-md mt-4">
+          <div className="mb-6 items-center justify-between flex">
+            <h2 className="text-xl font-semibold text-gray-700">
+              Selected Files
+            </h2>
+            {(files.length > 0 || jsonData) && (
+              <button
+                onClick={reset}
+                className="bg-red-500 text-white py-2 px-4 rounded-lg hover:bg-red-600 transition-colors">
+                Reset
+              </button>
+            )}
+          </div>
+          <div className="mb-4 justify-center flex flex-wrap gap-4">
             {files.map((file, index) => (
-              <li key={index} className="text-gray-600">
-                {file.name}
-              </li>
+              <div
+                key={index}
+                className="text-gray-600 rounded-xl p-2 flex flex-col items-center space-y-2">
+                <Image
+                  src={URL.createObjectURL(file)}
+                  alt={file.name}
+                  width={128}
+                  height={64}
+                  className="object-cover rounded-lg"
+                />
+                <span>{file.name}</span>
+              </div>
             ))}
-          </ul>
+          </div>
           <button
             onClick={convertToBase64Images}
             className="w-full bg-blue-500 text-white py-2 rounded-lg hover:bg-blue-600 transition-colors">
@@ -113,8 +140,11 @@ export default function Home() {
       {jsonData && <div className="border"></div>}
 
       {jsonData && (
-        <div className="w-full max-w-lg bg-white p-4 mt-6 rounded-lg shadow-md">
-          <div className="mb-6 flex justify-end">
+        <div className="w-full max-w-[1000px] bg-white p-4 mt-6 rounded-lg shadow-md">
+          <div className="mb-6 items-center justify-between flex">
+            <h2 className="text-xl font-semibold text-gray-700 mb-4">
+              Converted Data (Preview)
+            </h2>
             <button
               onClick={downloadJson}
               className="w-full max-w-[100px] bg-slate-300 py-2 rounded-lg text-slate-600 hover:bg-slate-400 transition-colors mt-4">
@@ -122,12 +152,9 @@ export default function Home() {
             </button>
           </div>
 
-          <h2 className="text-xl font-semibold text-gray-700 mb-4">
-            Converted Data (Preview)
-          </h2>
           {jsonData.map((data, index) => {
-            const fileName = Object.keys(data)[0];
-            const base64Content = Object.values(data)[0];
+            const fileName = data.fileName;
+            const base64Content = data.content;
             return (
               <div key={index} className="mb-4">
                 <div className="flex justify-between items-center mb-2">
@@ -143,9 +170,19 @@ export default function Home() {
                     {lastClicked === index ? 'Copied' : 'Copy Content'}
                   </button>
                 </div>
-                <pre className="bg-gray-100 p-2 rounded-lg text-black text-sm overflow-auto max-h-32">
-                  {base64Content}
-                </pre>
+                <div className="flex flex-col md:flex-row items-start md:items-center">
+                  {/* Display the Base64 image preview */}
+                  <Image
+                    src={base64Content}
+                    alt={fileName}
+                    width={128}
+                    height={128}
+                    className="object-cover rounded-lg mr-4 mb-2 md:mb-0"
+                  />
+                  <pre className="bg-gray-100 p-2 rounded-lg text-black text-sm overflow-auto max-h-32">
+                    {base64Content}
+                  </pre>
+                </div>
               </div>
             );
           })}
